e8d24efb18f061bb3f24216f26a092ff64dcfa55
BrunaBarbaresco <44330308+BrunaBarbaresco@users.noreply.github.com>
2024-06-06 17:47:02 -0300

    Improves API functions with `try/catch`

--- api/index.js
+++ api/index.js
@@ -9,21 +9,29 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/api/transaction", async (req, res) => {
-  await mongoose.connect(process.env.MONGO_URL);
-  const { name, description, datetime, price } = req.body;
-  const transaction = await Transaction.create({
-    name,
-    description,
-    datetime,
-    price,
-  });
-  res.json(transaction);
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    const { name, description, datetime, price } = req.body;
+    const transaction = await Transaction.create({
+      name,
+      description,
+      datetime,
+      price,
+    });
+    res.json(transaction);
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
 app.get("/api/transactions", async (req, res) => {
-  await mongoose.connect(process.env.MONGO_URL);
-  const transactions = await Transaction.find();
-  res.json(transactions);
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    const transactions = await Transaction.find();
+    res.json(transactions);
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
 app.listen(4000);
